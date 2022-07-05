94d7b26aedccf405c53b271716acc1bd740e71ef
sex0090 <107782906+sex0090@users.noreply.github.com>
2022-07-05 14:39:06 +0600

    bypass.py

--- bypass.py
+++ bypass.py
@@ -8,14 +8,14 @@ import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
     print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools")
-    ban='''
+    print("
 \033[1;32m███████╗███████╗██╗  ██╗
 \033[1;35m██╔════╝██╔════╝╚██╗██╔╝
 \033[1;34m███████╗█████╗   ╚███╔╝
 \033[1;36m╚════██║██╔══╝   ██╔██╗ 
 \033[1;33m███████║███████╗██╔╝ ██╗
 \033[1;35m╚══════╝╚══════╝╚═╝  ╚═╝
-\033[41m\033[1;37m[M31THUN]   [ADNAM]   [RJ_MAMUN]   [INSAN]\x1b[0m '''
+\033[41m\033[1;37m[M31THUN]   [ADNAM]   [RJ_MAMUN]   [INSAN]\x1b[0m ")
     #os.system('xdg-open https://www.facebook.com/groups/1136477216940696/');time.sleep(2)
     from AKING import Menu
     Menu()
